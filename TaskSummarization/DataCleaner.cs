bfb15f21a14de2eac9dbbf3ba1167c92e85b53fe
Patrick Gousseau <pgousse@cs.students.ubc.ca>
2021-05-25 08:32:36 -0500

    Added bag of words functionality

--- TaskSummarization/DataCleaner.cs
+++ TaskSummarization/DataCleaner.cs
@@ -3,6 +3,7 @@ using Microsoft.ML;
 using NHunspell;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,31 +15,65 @@ namespace TaskSummarization
         class DataCleaner
         {
 
-            /// <summary>
-            /// Cleans data set 
-            /// </summary>
-            /// <param name="data"></param>
-            /// <returns></returns>
-            public List<List<string>> clean(List<string> data)
-            {
-                List<List<string>> cleanedText = new List<List<string>>();
+        MLContext context = new MLContext();
 
-                foreach (string timeBlock in data)
+        /// <summary>
+        /// Cleans data set 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<List<string>>> clean(List<List<string>> data)
+            {
+                List<List<List<string>>> cleanedText = new List<List<List<string>>>();
+
+                foreach (List<string> timeBlock in data)
                 {
-                    // tokenize, remove stop words, stem and remove non english words
-                    List<string> cleanedTimeBlock = removeNonEnglishWordsAndStem(tokenize(timeBlock));
+
+                    List<List<string>> cleanedTimeBlock = new List<List<string>>();
+                    foreach (string windowTitle in timeBlock)
+                    {
+                        // tokenize, remove stop words, stem and remove non english words
+                        List<string> cleanedWindowTitle = removeNonEnglishWordsAndStem(tokenize(windowTitle)); // list of tokens based off the window title
+                        cleanedTimeBlock.Add(cleanedWindowTitle);
+                        
+                    }
                     cleanedText.Add(cleanedTimeBlock);
+
                 }
-                return cleanedText;
+            test();
+
+            return cleanedText;
             }
 
+        public void test()
+        {
+            //List<List<string>> list = Queries.GetWindowTitles(60, _date);
+            string filePath = @"C:\Users\pcgou\OneDrive\Documents\Work\test.txt";
 
-            /// <summary>
-            /// Removes non English words and stems each word
-            /// </summary>
-            /// <param name="words"></param>
-            /// <returns></returns>
-            private List<string> removeNonEnglishWordsAndStem(string[] words)
+            //StreamWriter writer = new StreamWriter(filePath);
+            //writer.WriteLine("testing");
+
+            File.WriteAllText(filePath, "testingtesting");
+
+            //foreach(List<string> timeBlock in list)
+            //{
+            //    foreach(string title in timeBlock)
+            //    {
+            //        await File.WriteAllTextAsync(filePath, title);
+            //        File.WriteAllText(filePath, "test");
+            //    }
+            //}
+
+
+        }
+
+
+        /// <summary>
+        /// Removes non English words and stems each word
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        private List<string> removeNonEnglishWordsAndStem(string[] words)
             {
                 EnglishStemmer stemmer = new EnglishStemmer();
                 Hunspell hunspell = new Hunspell("en_us.aff", "en_us.dic");
@@ -62,7 +97,7 @@ namespace TaskSummarization
             private string[] tokenize(string text)
             {
 
-                var context = new MLContext();
+                
                 var emptyData = new List<TextData>();
                 var data = context.Data.LoadFromEnumerable(emptyData);
 
@@ -79,6 +114,58 @@ namespace TaskSummarization
                 return newText.Tokens;
 
             }
+
+        /// <summary>
+        /// Creates a list of bags of words
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<Dictionary<string, int>> createBagOfWords(List<List<List<string>>> data)
+        {
+
+            List<Dictionary<string, int>> bagsOfWords = new List<Dictionary<string, int>>();
+
+            foreach(List<List<string>> timeBlock in data)
+            {
+                Dictionary<string, int> bag = new Dictionary<string, int>();
+                foreach(List<string> windowTitles in timeBlock)
+                {
+                    foreach(string token in windowTitles)
+                    {
+                        
+                        if(bag.ContainsKey(token))
+                        {
+                            bag[token] = bag[token] + 1;
+                        } else
+                        {
+                            bag.Add(token, 1);
+                        }
+                    }
+                }
+                bagsOfWords.Add(bag);
+            }
+            return bagsOfWords;
+        }
+
+        /// <summary>
+        /// Deletes the least occuring words based on topPercentile
+        /// </summary>
+        /// <param name="bagOfWords"></param>
+        /// /// <param name="topPercentile"></param>
+        /// <returns></returns>
+        public List<KeyValuePair<string, int>> getImportantWords(Dictionary<string,int> bagOfWords, float topPercentile)
+        {
+            var importantWords = bagOfWords.ToList();
+
+            importantWords.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            int upperBound = (int)(importantWords.Count * (1 - topPercentile));
+
+            importantWords.RemoveRange(0, upperBound);
+
+            return importantWords;
+        }
+            
 
 
             private class TextData
--- TaskSummarization/Program.cs
+++ TaskSummarization/Program.cs
@@ -10,21 +10,50 @@ namespace TaskSummarization
     {
         static void Main(string[] args)
         {
-            List<string> temp = new List<string> { "testing is a great fun deal tree", "test1 hi nope recursion badd children fishing" };
-            
-           
+ 
+            List<string> a = new List<string>() { "AM.PA.MonitoringTool (Running) - Microsoft Visual Studio", "New notification", "AM.PA.MonitoringTool (Running) - Microsoft Visual Studio", "Untitled - Google Chrome", "Your Retrospection for the 5/21/2021 - Google Chrome------AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---------PersonalAnalytics: Retrospection & Insights---Your Retrospection for the 5/21/2021 - Google Chrome---file io visual studio does not work if non console application - Google Search - Google Chrome---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification---------PersonalAnalytics: Retrospection & Insights" };
+            List<string> b = new List<string>() { "PersonalAnalytics: Retrospection & Insights---file io visual studio does not work if non console application - Google Search - Google Chrome---C# Write To File - Google Chrome---file io visual studio does not work if non console application - Google Search - Google Chrome---Enumerable.ToList(IEnumerable) Method (System.Linq) | Microsoft Docs - Google Chrome" };
+            List<string> c = new List<string>() { "Enumerable.ToList(IEnumerable) Method (System.Linq) | Microsoft Docs - Google Chrome---PersonalAnalytics: Retrospection & Insights" };
+            List<string> d = new List<string>() { "PersonalAnalytics: Retrospection & Insights---Enumerable.ToList(IEnumerable) Method (System.Linq) | Microsoft Docs - Google Chrome" };
+            List<string> e = new List<string>() { "Enumerable.ToList(IEnumerable) Method (System.Linq) | Microsoft Docs - Google Chrome---csatterfield/TaskDetection-analysis - Google Chrome" };
+            List<string> f = new List<string>() { "csatterfield/TaskDetection-analysis - Google Chrome---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification---------PersonalAnalytics: Retrospection & Insights---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---csatterfield/TaskDetection-analysis - Google Chrome--- | Proceedings of the SIGCHI Conference on Human Factors in Computing Systems - Google Chrome---Cosine similarity - Wikipedia - Google Chrome" };
+            List<string> g = new List<string>() { "Cosine similarity - Wikipedia - Google Chrome" };
+            List<string> h = new List<string>() { "Cosine similarity - Wikipedia - Google Chrome--- | Proceedings of the SIGCHI Conference on Human Factors in Computing Systems - Google Chrome---visual studio print to console on non console application c# - Google Search - Google Chrome---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification" };
+            List<string> i = new List<string>() { "New notification---------PersonalAnalytics: Retrospection & Insights---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification---------PersonalAnalytics: Retrospection & Insights---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio" };
+            List<string> j = new List<string>() { "AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---visual studio print to console on non console application c# - Google Search - Google Chrome---C# Read and Write to a Text File - YouTube - Google Chrome---visual studio print to console on non console application c# - Google Search - Google Chrome--- | Proceedings of the SIGCHI Conference on Human Factors in Computing Systems - Google Chrome" };
+            List<string> k = new List<string>() { " | Proceedings of the SIGCHI Conference on Human Factors in Computing Systems - Google Chrome---visual studio print to console on non console application c# - Google Search - Google Chrome---Enumerable.ToList<TSource>(IEnumerable<TSource>) Method (System.Linq) | Microsoft Docs - Google Chrome---c# - What is the best way to iterate over a dictionary? - Stack Overflow - Google Chrome---c# - How do you sort a dictionary by value? - Stack Overflow - Google Chrome---New Tab - Google Chrome---Outlook - Google Chrome---Loading... - Google Chrome---Mail - pgousse@student.ubc.ca - Google Chrome---c# - How do you sort a dictionary by value? - Stack Overflow - Google Chrome" };
+            List<string> l = new List<string>() { "c# - How do you sort a dictionary by value? - Stack Overflow - Google Chrome---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification---------PersonalAnalytics: Retrospection & Insights---AM.PA.MonitoringTool (Running) - Microsoft Visual Studio---New notification---------PersonalAnalytics: Retrospection & Insights" };
+
+
+
+
+            List <List<string>> data = new List<List<string>>();
+            data.Add(a);
+            data.Add(b);
+            data.Add(c);
+            data.Add(d);
+            data.Add(e);
+            data.Add(f);
+            data.Add(g);
+            data.Add(h);
+            data.Add(i);
+            data.Add(j);
+            data.Add(k);
+            data.Add(l);
+            //data.Add(temp1);
 
             DataCleaner cleaner = new DataCleaner();
-            cleaner.clean(temp);
-            List<List<string>> test = cleaner.clean(temp);
+            List<Dictionary<string, int>> cleaned = cleaner.createBagOfWords(cleaner.clean(data));
 
-            foreach(List<string> list in test)
+
+            foreach(Dictionary<string, int> dict in cleaned)
             {
-                foreach(string x in list)
+                List<KeyValuePair<string, int>> topWords = cleaner.getImportantWords(dict, (float)0.6);
+                foreach (KeyValuePair<string, int> entry in topWords)
                 {
-                    Console.WriteLine(x);
-                    
+                    Console.WriteLine(entry.Key + ": " + entry.Value);
                 }
+                Console.WriteLine("----------------");
             }
 
             Console.ReadLine();
