c35f80b864a51fca7022d4cad5d6216d3afcfd5f
Patrick Gousseau <pcgousseau@gmail.com>
2021-07-26 15:21:23 -0500

    refactored gt tasks

--- TaskSummarization/Tester.cs
+++ TaskSummarization/Tester.cs
@@ -15,10 +15,9 @@ namespace TaskSummarization
         private string participant;
         private Summarizer summarizer;
         private List<Task> tasks;
-        private List<KeyValuePair<int[], int>> correctData;
+        private List<Task> groundTruthTasks;
         private Dictionary<int, Dictionary<int,int>> GTtaskMatchingDict;
         private Dictionary<int, Dictionary<int, int>> outputTaskMatchingDict;
-        private Dictionary<int, Task> GroundTruthDescriptions;
         
 
         public Tester(int numSecs, double topPercentile, double similarityThreshold, string participant)
@@ -27,15 +26,14 @@ namespace TaskSummarization
             this.topPercentile = topPercentile;
             this.similarityThreshold = similarityThreshold;
             this.participant = participant;
-            GroundTruthDescriptions = new Dictionary<int, Task>();
             GTtaskMatchingDict = new Dictionary<int, Dictionary<int, int>>();
-            outputTaskMatchingDict = new Dictionary<int, Dictionary<int, int>>(); 
+            outputTaskMatchingDict = new Dictionary<int, Dictionary<int, int>>();
+            this.groundTruthTasks = new List<Task>();
 
             initializeSummarizer();
-            setGTDescriptions();
 
             this.tasks = summarizer.getTasks();
-            this.correctData = getCorrectData();
+            getGroundTruthData();
         }
 
         /// <summary>
@@ -51,6 +49,7 @@ namespace TaskSummarization
             foreach (List<string> titles in data)
             {
                 Task task = new Task(titles, topPercentile);
+               
                 summarizer.addTask(task);
 
             }
@@ -68,27 +67,27 @@ namespace TaskSummarization
             float correctHorizPoints = 0;
             float correctVertPoints = 0;
           
-            foreach (KeyValuePair<int[], int>  GTtask in correctData)
+            foreach (Task GTtask in groundTruthTasks)
             {
-                int startSecs = GTtask.Key[0];
+                int startSecs = GTtask.getStartTime();
                 TimeSpan startTime = TimeSpan.FromSeconds(startSecs);
                 string startHours = startTime.ToString(@"hh\:mm\:ss");
 
-                int endSecs = GTtask.Key[1];
+                int endSecs = GTtask.getEndTime();
                 TimeSpan endTime = TimeSpan.FromSeconds(endSecs);
                 string endHours = endTime.ToString(@"hh\:mm\:ss");
 
 
                 // Populate GTtaskMatchingDict
 
-                int outputTaskNum = mostCommonTaskNumInOutput(startSecs, endSecs); // Most common task number in the output within the interval of GTtask
+                int outputTaskNum = mostCommonTaskNumInInterval(startSecs, endSecs, tasks); // Most common task number in the output within the interval of GTtask
 
-                if(GTtaskMatchingDict.ContainsKey(GTtask.Value))
+                if(GTtaskMatchingDict.ContainsKey(GTtask.getTaskNum()))
                 {
-                    addToDict(GTtaskMatchingDict[GTtask.Value],outputTaskNum);
+                    addToDict(GTtaskMatchingDict[GTtask.getTaskNum()],outputTaskNum);
                 } else
                 {
-                    GTtaskMatchingDict.Add(GTtask.Value, new Dictionary<int, int> { { outputTaskNum, 1 } });
+                    GTtaskMatchingDict.Add(GTtask.getTaskNum(), new Dictionary<int, int> { { outputTaskNum, 1 } });
                 }
 
                 // Vertical and Horizontal test
@@ -126,7 +125,7 @@ namespace TaskSummarization
         {
             foreach(Task task in tasks)
             {
-                int outputTaskNum = mostCommonTaskNumInGT(task.getStartTime(), task.getEndTime()); // Most common task number in GT within the interval of task
+                int outputTaskNum = mostCommonTaskNumInInterval(task.getStartTime(), task.getEndTime(), groundTruthTasks); // Most common task number in GT within the interval of task
 
                 if (outputTaskMatchingDict.ContainsKey(task.getTaskNum()))
                 {
@@ -195,50 +194,50 @@ namespace TaskSummarization
             return null; // transition period
         }
 
-        private int mostCommonTaskNumInGT(int outputStartTime, int outputEndTime)
+        //private int mostCommonTaskNumInGT(int outputStartTime, int outputEndTime)
+        //{
+        //    int mostCommon = -1;
+        //    int currentMax = 0;
+        //    foreach (KeyValuePair<int[],int> GTtask in correctData)
+        //    {
+        //        int GTStartTime = GTtask.Key[0];
+        //        int GTputEndTime = GTtask.Key[1];
+        //        int numTimeBlocks = 0;
+
+        //        if (GTStartTime >= outputStartTime && GTputEndTime <= outputEndTime) // GT block is within output
+        //        {
+        //            numTimeBlocks = (GTputEndTime - GTStartTime) / numSecs;
+
+        //        }
+        //        else if (GTStartTime >= outputStartTime && GTStartTime <= outputEndTime) // GT block begins in output and ends after it
+        //        {
+        //            numTimeBlocks = (outputEndTime - GTStartTime) / numSecs;
+
+        //        }
+        //        else if (GTputEndTime >= outputStartTime && GTputEndTime <= outputEndTime) // GT block begins before output and ends inside of it
+        //        {
+        //            numTimeBlocks = (GTputEndTime - outputStartTime) / numSecs;
+
+        //        }
+        //        else if (GTStartTime <= outputStartTime && GTputEndTime >= outputEndTime) // GT block begins before output and ends after it
+        //        {
+        //            numTimeBlocks = (outputEndTime - outputStartTime) / numSecs;
+        //        }
+
+        //        if (numTimeBlocks > currentMax)
+        //        {
+        //            currentMax = numTimeBlocks;
+        //            mostCommon = GTtask.Value;
+        //        }
+        //    }
+        //    return mostCommon;
+        //}
+
+        private int mostCommonTaskNumInInterval(int GTStartTime, int GTEndTime, List<Task> taskList)
         {
             int mostCommon = -1;
             int currentMax = 0;
-            foreach (KeyValuePair<int[],int> GTtask in correctData)
-            {
-                int GTStartTime = GTtask.Key[0];
-                int GTputEndTime = GTtask.Key[1];
-                int numTimeBlocks = 0;
-
-                if (GTStartTime >= outputStartTime && GTputEndTime <= outputEndTime) // GT block is within output
-                {
-                    numTimeBlocks = (GTputEndTime - GTStartTime) / numSecs;
-
-                }
-                else if (GTStartTime >= outputStartTime && GTStartTime <= outputEndTime) // GT block begins in output and ends after it
-                {
-                    numTimeBlocks = (outputEndTime - GTStartTime) / numSecs;
-
-                }
-                else if (GTputEndTime >= outputStartTime && GTputEndTime <= outputEndTime) // GT block begins before output and ends inside of it
-                {
-                    numTimeBlocks = (GTputEndTime - outputStartTime) / numSecs;
-
-                }
-                else if (GTStartTime <= outputStartTime && GTputEndTime >= outputEndTime) // GT block begins before output and ends after it
-                {
-                    numTimeBlocks = (outputEndTime - outputStartTime) / numSecs;
-                }
-
-                if (numTimeBlocks > currentMax)
-                {
-                    currentMax = numTimeBlocks;
-                    mostCommon = GTtask.Value;
-                }
-            }
-            return mostCommon;
-        }
-
-        private int mostCommonTaskNumInOutput(int GTStartTime, int GTEndTime)
-        {
-            int mostCommon = -1;
-            int currentMax = 0;
-            foreach (Task task in tasks)
+            foreach (Task task in taskList)
             {
                 int outputStartTime = task.getStartTime();
                 int outputEndTime = task.getEndTime();
@@ -282,12 +281,12 @@ namespace TaskSummarization
             return false; // transition period
         }
 
-        private int verticalTest(int startSecs, int endSecs, int taskNum)
+        private int verticalTest(int startSecs, int endSecs, Task task)
         {
             int points = 0;
             if (isHomogeneous(startSecs + 150, endSecs - 150))
             {
-                if (summarizer.similarTasks(getTaskAtTime(startSecs + 150, endSecs - 150), GroundTruthDescriptions[taskNum], 0.3))
+                if (summarizer.similarTasks(getTaskAtTime(startSecs + 150, endSecs - 150), task, 0.3))
                 {
                     points = 1;
                 }
@@ -317,13 +316,13 @@ namespace TaskSummarization
         /// Fetches ground truth data
         /// </summary>
         /// <returns></returns>
-        public List<KeyValuePair<int[], int>> getCorrectData()
+        public void getGroundTruthData()
         {
             string format = "g";
             var culture = CultureInfo.InvariantCulture;
             List<KeyValuePair<int[], int>> truth = new List<KeyValuePair<int[], int>>();
             string path = @"c:\users\pcgou\onedrive\documents\ubcresearch\window_titles\";
-           path += participant;
+            path += participant;
             path += @"\truth.txt";
             //string path = @"C:\Users\pcgou\OneDrive\Documents\UBCResearch\window_titles\PD\truth.txt";
             try
@@ -331,24 +330,20 @@ namespace TaskSummarization
                 string[] lines = File.ReadAllLines(path);
                 for (int i = 1; i < lines.Length; i++)
                 {
-                    //Console.WriteLine(lines.Length);
+                    
                     string[] items = lines[i].Split(',');
-
-                    int[] time = new int[2]; // time[0] = start time, time[1] = end time 
-                    TimeSpan start;
-                    TimeSpan end;
+                    Task newTask = new Task(getGTDescriptions(Convert.ToInt32(items[2])),1);
+                    TimeSpan start, end;
                     TimeSpan.TryParseExact(items[0], format, culture, TimeSpanStyles.AssumeNegative, out start);
                     TimeSpan.TryParseExact(items[1], format, culture, TimeSpanStyles.AssumeNegative, out end);
-                    time[0] = Convert.ToInt32(start.TotalSeconds);
-                    time[1] = Convert.ToInt32(end.TotalSeconds);
-                    
-                    truth.Add(new KeyValuePair<int[], int>(time, Convert.ToInt32(items[2])));
+                    newTask.setTimes(Convert.ToInt32(start.TotalSeconds), Convert.ToInt32(end.TotalSeconds));
+                    newTask.setTaskNum(Convert.ToInt32(items[2]));
+                    groundTruthTasks.Add(newTask);
                 }
 
             }
             catch (Exception e)
-            {
-                
+            {                
                 Console.WriteLine(e.Message);
             }
 
@@ -360,11 +355,7 @@ namespace TaskSummarization
 
             //}
             //Console.ReadLine();
-
-            return truth;
         }
-
-
 
         /// <summary>
         /// Fetches testing data
@@ -510,28 +501,25 @@ namespace TaskSummarization
         /// Initalizes GroundTruthDescriptions
         /// </summary>
         /// <returns></returns>
-        private void setGTDescriptions()
+        private List<string> getGTDescriptions(int taskNum)
         {
-            List<string> descriptions = new List<string>();
-            Task task1 = new Task(new List<string> { "Your manager has noticed that there has been a substantial influx of duplicate bug reports recently. Explore the provided list of bug reports, and identify whether there is a duplicate and provide its ID. search the bugs with the ids: 2264, 2268, 2271, 2277" }, 1);
-            Task task2 = new Task(new List<string> { "Your team has developed an application for optimizing developer work patterns - reducing the number of impactful interruptions. You have been tasked with creating visualizations for a presentation outlining the benefits of your product to potential clients. You have the following data available to you for use: Application usage times, interruption times, durations, and disruptiveness levels, keyboard and mouse activity levels. What libraries would you suggest for creating data representations? Give some examples of other works that have been created using these libraries. At a minimum the visualizations should include a before/after comparison of developers work days using the product vs not using the product." }, 1);
-            Task task3 = new Task(new List<string> { "The software company you work for is considering expanding into the productivity tool sphere. Your manager has asked you to do some market research on 3 of the most popular already existing apps in this domain: Microsoft To-do, Wunderlist, and Todoist. Provide a short written summary of the similarities and differences between these 3 apps." }, 1);
-            Task task4 = new Task(new List<string> { "Your coworker is having difficulty deciding on which productivity app they should use, and have asked you for a recommendation. They have narrowed their decision to 3 apps: Microsoft To-do, Wunderlist, and Todoist. Based only on app store reviews, which of these apps would you recommend? Identify any reviews that were particularly influential in your decision." }, 1);
-            Task task5 = new Task(new List<string> { "You are preparing to give a presentation on potential deep learning applications to the CTO of your company. While you have already completed the slides for the presentation, you should also prepare answers for a few questions which are likely to arise during the presentation. The lines drawn between layers of the network on the included slide represent weighted inputs from one layer to the next. How does the network decide on what weights to choose during the training process? Most of the technologies behind deep learning have already been around for over 30 years.Why is deep learning only becoming popular now ? What has changed ? What kind of performance increases can be seen by using GPU's instead of CPU's? Are GPU's always superior with respect to deep learning applications?" }, 1);
-            Task task6 = new Task(new List<string> { "You recently gave a short presentation to your colleagues outlining different ways the company may be able to make use of blockchain. One of your co-workers felt a little bit lost during the presentation, and emailed you a couple follow up questions afterwards. You mentioned that blockchain is a form of distributed ledger. What does this mean? What advantages are offered over traditional client-server database ledger systems? Could you explain what proof-of-work means? What is it? What does it do? Why is it necessary ?" }, 1);
-
-            //Task task1 = new Task(new List<string> { "Clean up code in Microsoft Visual Code" }, 1);
-            //Task task2 = new Task(new List<string> { "Transfer data from Notepad to Excel. Manipulate the data to find correlations" }, 1);
-            //Task task3 = new Task(new List<string> { "Find out if I should take a trip to Peru or Portugal. Look at Covid and vaccination profile to make sure it is safe" }, 1);
-            //Task task4 = new Task(new List<string> { "Read emails and reply to emails" }, 1);
-            //Task task5 = new Task(new List<string> { "Run a test in Visual Studio and transfer the data from Notepad to Excel " }, 1);
-
-            this.GroundTruthDescriptions.Add(1, task1);
-            this.GroundTruthDescriptions.Add(2, task2);
-            this.GroundTruthDescriptions.Add(3, task3);
-            this.GroundTruthDescriptions.Add(4, task4);
-            this.GroundTruthDescriptions.Add(5, task5);
-            this.GroundTruthDescriptions.Add(6, task6);
+            switch (taskNum)
+            {
+                case 1:
+                    return new List<string> { "Your manager has noticed that there has been a substantial influx of duplicate bug reports recently. Explore the provided list of bug reports, and identify whether there is a duplicate and provide its ID. search the bugs with the ids: 2264, 2268, 2271, 2277" };
+                case 2:
+                    return new List<string> { "Your team has developed an application for optimizing developer work patterns - reducing the number of impactful interruptions. You have been tasked with creating visualizations for a presentation outlining the benefits of your product to potential clients. You have the following data available to you for use: Application usage times, interruption times, durations, and disruptiveness levels, keyboard and mouse activity levels. What libraries would you suggest for creating data representations? Give some examples of other works that have been created using these libraries. At a minimum the visualizations should include a before/after comparison of developers work days using the product vs not using the product." };
+                case 3:
+                    return new List<string> { "The software company you work for is considering expanding into the productivity tool sphere. Your manager has asked you to do some market research on 3 of the most popular already existing apps in this domain: Microsoft To-do, Wunderlist, and Todoist. Provide a short written summary of the similarities and differences between these 3 apps." };
+                case 4:
+                    return new List<string> { "Your coworker is having difficulty deciding on which productivity app they should use, and have asked you for a recommendation. They have narrowed their decision to 3 apps: Microsoft To-do, Wunderlist, and Todoist. Based only on app store reviews, which of these apps would you recommend? Identify any reviews that were particularly influential in your decision." };
+                case 5:
+                    return new List<string> { "You are preparing to give a presentation on potential deep learning applications to the CTO of your company. While you have already completed the slides for the presentation, you should also prepare answers for a few questions which are likely to arise during the presentation. The lines drawn between layers of the network on the included slide represent weighted inputs from one layer to the next. How does the network decide on what weights to choose during the training process? Most of the technologies behind deep learning have already been around for over 30 years.Why is deep learning only becoming popular now ? What has changed ? What kind of performance increases can be seen by using GPU's instead of CPU's? Are GPU's always superior with respect to deep learning applications?" };
+                case 6:
+                    return new List<string> { "You recently gave a short presentation to your colleagues outlining different ways the company may be able to make use of blockchain. One of your co-workers felt a little bit lost during the presentation, and emailed you a couple follow up questions afterwards. You mentioned that blockchain is a form of distributed ledger. What does this mean? What advantages are offered over traditional client-server database ledger systems? Could you explain what proof-of-work means? What is it? What does it do? Why is it necessary ?" };
+                default:
+                    return null;
+            }
         }
 
         #endregion
